8371613e6b344dc6893436a2467432baf1ed95d4
cedi <cedric_christen@bluewin.ch>
2016-03-14 13:14:30 +0100

    plugin simplified and works now with ribbon gui

--- ovl_reader.py
+++ ovl_reader.py
@@ -20,16 +20,16 @@
  *                                                                         *
  ***************************************************************************/
 """
-from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
-from PyQt4.QtGui import QAction, QIcon
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 # Initialize Qt resources from file resources.py
 import resources
 # Import the code for the dialog
-from ovl_reader_dialog import OvlReaderDialog
+from qgsvbsovlimporter import QgsVBSOvlImporter
 import os.path
 
 
-class OvlReader:
+class OvlReader(QObject):
     """QGIS Plugin Implementation."""
 
     def __init__(self, iface):
@@ -40,10 +40,11 @@ class OvlReader:
             application at run time.
         :type iface: QgsInterface
         """
+        QObject.__init__(self)
         # Save reference to the QGIS interface
         self.iface = iface
-        # initialize plugin directory
         self.plugin_dir = os.path.dirname(__file__)
+
         # initialize locale
         locale = QSettings().value('locale/userLocale')[0:2]
         locale_path = os.path.join(
@@ -58,136 +59,33 @@ class OvlReader:
             if qVersion() > '4.3.3':
                 QCoreApplication.installTranslator(self.translator)
 
-        # Create the dialog (after translation) and keep reference
-        self.dlg = OvlReaderDialog()
-
-        # Declare instance attributes
-        self.actions = []
-        self.menu = self.tr(u'&Ovl Reader')
-        # TODO: We are going to let the user set this up in a future iteration
-        self.toolbar = self.iface.addToolBar(u'OvlReader')
-        self.toolbar.setObjectName(u'OvlReader')
-
-    # noinspection PyMethodMayBeStatic
-    def tr(self, message):
-        """Get the translation for a string using Qt translation API.
-
-        We implement this ourselves since we do not inherit QObject.
-
-        :param message: String for translation.
-        :type message: str, QString
-
-        :returns: Translated version of message.
-        :rtype: QString
-        """
-        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
-        return QCoreApplication.translate('OvlReader', message)
-
-
-    def add_action(
-        self,
-        icon_path,
-        text,
-        callback,
-        enabled_flag=True,
-        add_to_menu=True,
-        add_to_toolbar=True,
-        status_tip=None,
-        whats_this=None,
-        parent=None):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        if add_to_toolbar:
-            self.toolbar.addAction(action)
-
-        if add_to_menu:
-            self.iface.addPluginToMenu(
-                self.menu,
-                action)
-
-        self.actions.append(action)
-
-        return action
+        self.toolAction = None
 
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
 
-        icon_path = ':/plugins/OvlReader/icon.png'
-        self.add_action(
-            icon_path,
-            text=self.tr(u'Read ovl Files'),
-            callback=self.run,
-            parent=self.iface.mainWindow())
-
+        ovlAction = self.iface.findAction("mActionImportOVL")
+        if ovlAction:
+            ovlAction.triggered.connect(self.__importOVL)
+        else:
+            self.toolButton = QToolButton(self.iface.pluginToolBar())
+            self.toolButton.setIcon(QIcon(":/plugins/OvlReader/icon.png"))
+            self.toolButton.setText(self.tr("OVL Import"))
+            self.toolButton.setToolTip(self.tr("OVL Import"))
+            self.toolAction = self.iface.pluginToolBar().addWidget(self.toolButton)
+            self.toolButton.toggled.connect(self.__importOVL)
 
     def unload(self):
         """Removes the plugin menu item and icon from QGIS GUI."""
-        for action in self.actions:
-            self.iface.removePluginMenu(
-                self.tr(u'&Ovl Reader'),
-                action)
-            self.iface.removeToolBarIcon(action)
-        # remove the toolbar
-        del self.toolbar
+        if hasattr(self, 'toolButton'):
+            self.iface.pluginToolBar().removeAction(self.toolAction)
 
+    def __importOVL(self):
+        lastProjectDir = QSettings().value("/UI/lastProjectDir", ".")
+        filename = QFileDialog.getOpenFileName(self.iface.mainWindow(), self.tr("Select OVL File..."), lastProjectDir, self.tr("OVL Files (*.ovl);;"))
+        finfo = QFileInfo(filename)
+        if not finfo.exists():
+            return
 
-    def run(self):
-        """Run method that performs all the real work"""
-        # show the dialog
-        self.dlg.show()
-        self.dlg.iface = self.iface
-        # Run the dialog event loop
-        result = self.dlg.exec_()
-        # See if OK was pressed
-        if result:
-            # Do something useful here - delete the line containing pass and
-            # substitute with your code.
-            pass
+        importer = QgsVBSOvlImporter()
+        importer.import_ovl(filename, self.iface)

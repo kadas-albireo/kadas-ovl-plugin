a288158131a37fa6f83b324cf93687062e19e9ff
Sandro Mani <manisandro@gmail.com>
2016-06-16 19:46:52 +0200

    Add french translation

--- OvlReader/i18n/OvlReader_fr.ts
+++ OvlReader/i18n/OvlReader_fr.ts
@@ -1,21 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0">
+<!DOCTYPE TS>
+<TS version="2.0" language="fr">
 <context>
     <name>OvlReader</name>
     <message>
         <location filename="../ovl_reader.py" line="72"/>
         <source>OVL Import</source>
-        <translation type="unfinished"></translation>
+        <translation>Importér OVL</translation>
     </message>
     <message>
         <location filename="../ovl_reader.py" line="83"/>
         <source>Select OVL File...</source>
-        <translation type="unfinished"></translation>
+        <translation>Sélectionner fichier OVL...</translation>
     </message>
     <message>
         <location filename="../ovl_reader.py" line="83"/>
         <source>OVL Files (*.ovl);;</source>
-        <translation type="unfinished"></translation>
+        <translation>Fichiers OVL (*.ovl);;</translation>
     </message>
 </context>
 <context>
@@ -23,62 +24,62 @@
     <message>
         <location filename="../qgsovlimporter.py" line="42"/>
         <source>Select OVL File</source>
-        <translation type="unfinished"></translation>
+        <translation>Sélectionner fichier OVL</translation>
     </message>
     <message>
         <location filename="../qgsovlimporter.py" line="42"/>
         <source>OVL Files (*.ovl);;</source>
-        <translation type="unfinished"></translation>
+        <translation>Fichiers OVL (*.ovl);;</translation>
     </message>
     <message>
         <location filename="../qgsovlimporter.py" line="57"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur</translation>
     </message>
     <message>
         <location filename="../qgsovlimporter.py" line="57"/>
         <source>The file does not appear to be a valid OVL v5.0 file:  {fn}</source>
-        <translation type="unfinished"></translation>
+        <translation>Le fichier ne semble pas être un fichier OVL v5.0 valide: {fn}</translation>
     </message>
     <message>
         <location filename="../qgsovlimporter.py" line="75"/>
         <source>Importing OVL...</source>
-        <translation type="unfinished"></translation>
+        <translation>Importation de OVL...</translation>
     </message>
     <message>
         <location filename="../qgsovlimporter.py" line="75"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Annuler</translation>
     </message>
     <message>
         <location filename="../qgsovlimporter.py" line="76"/>
         <source>OVL Import</source>
-        <translation type="unfinished"></translation>
+        <translation>Importér OVL</translation>
     </message>
     <message>
         <location filename="../qgsovlimporter.py" line="139"/>
         <source>Import completed</source>
-        <translation type="unfinished"></translation>
+        <translation>Importation terminé</translation>
     </message>
     <message>
         <location filename="../qgsovlimporter.py" line="141"/>
         <source>{cnt} features were imported</source>
-        <translation type="unfinished"></translation>
+        <translation>{cnt} objets ont été importés</translation>
     </message>
     <message>
         <location filename="../qgsovlimporter.py" line="143"/>
         <source>The following objects could not be converted:</source>
-        <translation type="unfinished"></translation>
+        <translation>Les objets suivants ne peuvent pas être convertis:</translation>
     </message>
     <message>
-        <location filename="../qgsovlimporter.py" line="152"/>
+        <location filename="../qgsovlimporter.py" line="156"/>
         <source>Please note that even for successfully converted objects, the representation may differ compared to the PCMAP Swissline Software. It is therefore recommended to check the imported data.</source>
-        <translation type="unfinished"></translation>
+        <translation>S&apos;il vous plaît noter que même pour les objets convertis avec succès, la représentation peut différer par rapport à l&apos;application Swissline PCMAP. Il est donc recommandé de vérifier les données importées.</translation>
     </message>
     <message>
         <location filename="../qgsovlimporter.py" line="148"/>
         <source>The following MSS symbols did not pass validation:</source>
-        <translation type="unfinished"></translation>
+        <translation>Les symboles du MSS suivants ne passent pas la validation:</translation>
     </message>
 </context>
 </TS>
